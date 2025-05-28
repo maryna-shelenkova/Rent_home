6acae7cdb6c31287cc8b089deac52e335f828e33
mshel <maryna.shelenkova@gmx.de>
2025-05-28 16:04:36 +0200

    Initial project structure and custom user model Add listings, bookings, and reviews models Implement permissions for bookings and reviews Add serializers and viewsets for all models Set up URLs for all apps and API routing Add booking and review validation logic Fix import and permission issues

--- Rent_home/urls.py
+++ Rent_home/urls.py
@@ -1,22 +1,21 @@
-"""
-URL configuration for Rent_home project.
+from django.contrib import admin
+from django.urls import path, include
+from listings import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from users.views import UserRegistrationView
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-# from django.contrib import admin
-from django.urls import path
 
 urlpatterns = [
-    #    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('api/users/', include('users.urls')),
+    path('api/listings/', include('listings.urls')),
+    path('api/bookings/', include('bookings.urls')),
+    path('api/reviews/', include('reviews.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/', UserRegistrationView.as_view(), name='user-register'),
 ]
+
